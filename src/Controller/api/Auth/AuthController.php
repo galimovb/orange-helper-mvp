e2ba20c73c4fcc077ed19d7c33d2b22b7cfefcda
Булат Галимов <b.galimov@bambit.ru>
2025-07-07 11:44:21 +0300

    Настроил : защиту роутов

--- src/Controller/api/Auth/AuthController.php
+++ src/Controller/api/Auth/AuthController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class AuthController extends AbstractController
 {
@@ -13,5 +15,14 @@ final class AuthController extends AbstractController
     public function login(): Response
     {
        return new JsonResponse(['message' => 'Успешно']);
+    }
+
+    #[Route('/api/auth/check', name: 'api_auth_check', methods: ['GET'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function me(UserInterface $user): JsonResponse
+    {
+        return $this->json([
+            true
+        ]);
     }
 }
